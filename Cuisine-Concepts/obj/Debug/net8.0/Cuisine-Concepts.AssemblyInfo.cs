79c8266051ecfbd54a11c8cc58ac40629e1e6f36
Hoyt Brem <hoytbrem@gmail.com>
2024-03-04 14:38:27 -0600

    worked on about us page

--- Cuisine-Concepts/obj/Debug/net8.0/Cuisine-Concepts.AssemblyInfo.cs
+++ Cuisine-Concepts/obj/Debug/net8.0/Cuisine-Concepts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cuisine-Concepts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e15ff3a5fb543461ae4667700312116d2d8f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22551a54a99ef757a390bd52bd17d2de5653023d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cuisine-Concepts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cuisine-Concepts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
