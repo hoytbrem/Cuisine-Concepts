9be4cfaad13abd1d65090c607a548e2dc3ea5772
Hoyt Brem <104306396+hoytbrem@users.noreply.github.com>
2024-03-05 16:29:39 -0600

    Delete Cuisine-Concepts directory

--- Cuisine-Concepts/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Cuisine_Concepts.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
